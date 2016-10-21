1567229c87f31f3c460a2e5265473538e1f6cb34
benallen <external@benallen.info>
2016-10-21 23:19:50 +0100

    Added menu and file / folder exporter

--- LingoTool Exporter.sketchplugin/Contents/Sketch/exporter-main.js
+++ LingoTool Exporter.sketchplugin/Contents/Sketch/exporter-main.js
@@ -57,7 +57,6 @@ function onRun(context) {
   function p_copyTextString(layer){
     if ([layer name] == [layer stringValue]){
       stringsMissingKeysArray.push([layer stringValue])
-      LTLog([layer stringValue])
       return
     }
 
@@ -65,7 +64,7 @@ function onRun(context) {
       "id" : [layer name],
       "string" : [layer stringValue]
     }
-    
+
     keyStringStore.push(keyStringObject)
   }
 
@@ -79,6 +78,61 @@ function prettifyStringsArray(unPrettyArray){
     workingString += " - " + unPrettyArray[i] + "\n"
   }
   return workingString
+}
+
+// File Utilities
+var writeTextToFile = function(text, filePath) {
+    var t = [NSString stringWithFormat:@"%@", text],
+    f = [NSString stringWithFormat:@"%@", filePath];
+    return [t writeToFile:f atomically:true encoding:NSUTF8StringEncoding error:nil];
+}
+
+var readTextFromFile = function(filePath) {
+    var fileManager = [NSFileManager defaultManager];
+    if([fileManager fileExistsAtPath:filePath]) {
+        return [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
+    }
+    return nil;
+}
+
+var jsonFromFile = function(filePath, mutable) {
+    var data = [NSData dataWithContentsOfFile:filePath];
+	var options = mutable == true ? NSJSONReadingMutableContainers : 0
+	return [NSJSONSerialization JSONObjectWithData:data options:options error:nil];
+}
+
+var saveJsonToFile = function(jsonObj, filePath) {
+    writeTextToFile(stringify(jsonObj), filePath);
+}
+
+var stringify = function(obj, prettyPrinted) {
+    var prettySetting = prettyPrinted ? NSJSONWritingPrettyPrinted : 0,
+    jsonData = [NSJSONSerialization dataWithJSONObject:obj options:prettySetting error:nil];
+    return [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+}
+
+var createTempFolderNamed = function(name) {
+    var tempPath = getTempFolderPath(name);
+    createFolderAtPath(tempPath);
+    return tempPath;
+}
+
+var getTempFolderPath = function(withName) {
+    var fileManager = [NSFileManager defaultManager],
+    cachesURL = [[fileManager URLsForDirectory:NSCachesDirectory inDomains:NSUserDomainMask] lastObject],
+    withName = (typeof withName !== 'undefined') ? withName : (Date.now() / 1000),
+    folderName = [NSString stringWithFormat:"%@", withName];
+    return [[cachesURL URLByAppendingPathComponent:folderName] path];
+}
+
+var createFolderAtPath = function(pathString) {
+    var fileManager = [NSFileManager defaultManager];
+    if([fileManager fileExistsAtPath:pathString]) return true;
+    return [fileManager createDirectoryAtPath:pathString withIntermediateDirectories:true attributes:nil error:nil];
+}
+
+var removeFileOrFolder = function(filePath) {
+    [[NSFileManager defaultManager] removeItemAtPath:filePath error:nil];
 }
 
 // Alert Functions
